fcfa48428e0359c0e3bf12cfbb081f0634591ec6
MateusKH <Mateus Kunz Hippler@192.168.10.12>
2023-06-20 15:29:35 -0300

    Dragons of Berk

---
+++ src/main/java/net/mcreator/dragon_of_berk/procedures/MMMProcedure.java
@@ -0,0 +1,9 @@
+package net.mcreator.dragon_of_berk.procedures;
+
+import net.mcreator.dragon_of_berk.DragonOfBerkMod;
+
+public class MMMProcedure {
+	public static void execute() {
+		DragonOfBerkMod.LOGGER.info(("text").length());
+	}
+}
